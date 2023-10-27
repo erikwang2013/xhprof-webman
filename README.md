731d4a0290230fb5273cc1cae79896ccfa22997a
erik <erik@erik.xyz>
2023-10-27 17:12:57 +0800

    更换配置文件

--- README.md
+++ README.md
@@ -9,20 +9,9 @@ composer require erikwang2013/xhprof-webman
 
 ## 配置 ##
 
-1. 在配置文件config中app.php添加开关
 
-```
-'xhprof'=>[
-        'enable'=>true,   //是否开启xhprof分析
-        'time_limit'=>0,  //仅记录响应超过多少秒的请求  默认0记录所有
-        'log_num'=>1000, //仅记录最近的多少次请求(最大值有待观察，看日志、查看响应时间) 默认1000
-        'view_wtred'=>3, //列表耗时超过多少秒标红 默认3s
-        'ignore_url_arr'=>["/test"],   //忽略URL配置
-    ]
 
-```
-
-2. 创建控制器,复制下面代码
+1. 创建控制器,复制下面代码
 
 ```
 <?php
@@ -42,12 +31,12 @@ class TestController
 
 ```
 
-3. 路由增加以下代码
+2. 路由增加以下代码
 ```
 Route::get('/test', ['app\controller\TestController','index']);
 
 ```
 
-4. 然后重启服务就可以访问了。
+3. 然后重启服务就可以访问了。
 ![](./doc/1.jpg)
 ![](./doc/2.jpg)
--- src/Webman/XhprofMiddleware.php
+++ src/Webman/XhprofMiddleware.php
@@ -15,12 +15,13 @@ class XhprofMiddleware implements MiddlewareInterface
 {
     public function process(Request $request, callable $next): Response
     {
-        $xhprof = config('app.xhprof')['enable']?:false;
+        $config=config('plugin.erikwang2013.xhprof.xhprof');
+        $xhprof =$config['enable']?:false;
         $extension = extension_loaded('xhprof');
-        Xhprof::$ignore_url_arr=config('app.xhprof')['ignore_url_arr']?:"/test";
-        Xhprof::$time_limit=config('app.xhprof')['time_limit']?:0;
-        Xhprof::$log_num=config('app.xhprof')['log_num']?:1000;
-        Xhprof::$view_wtred=config('app.xhprof')['view_wtred']?:3;
+        Xhprof::$ignore_url_arr=$config['ignore_url_arr']?:"/test";
+        Xhprof::$time_limit=$config['time_limit']?:0;
+        Xhprof::$log_num=$config['log_num']?:1000;
+        Xhprof::$view_wtred=$config['view_wtred']?:3;
         if ($xhprof && $extension) Xhprof::xhprofStart();
         $response = $next($request);
         if ($xhprof && $extension) Xhprof::xhprofStop();
